b493ed8f7b76442ceb80b230a1159b5a5687a81d
67160366 <67160366@go.buu.ac.th>
2025-10-08 11:22:54 +0700

    Add files via upload

---
+++ bubble_sort.py
@@ -0,0 +1,35 @@
+class BubbleSorter:
+    def __init__(self,data=None):
+        if data is None:
+            self.list = []
+        else:
+            self.list = data.copy()
+                   
+    def display(self):
+        print(self.list)
+
+    def sort(self):
+        n = len(self.list)
+
+        for i in range(n-1):
+            swapped = False
+            for j in range(n-1-i):
+                if self.list[j] > self.list[j+1]:
+                    self.list[j], self.list[j+1] = self.list[j+1], self.list[j]
+                    swapped = True
+            
+            print(f"After round{i+1}: {self.list}")
+            if not swapped:
+                break
+
+if __name__ == "__main__":
+    nums = [64, 34, 25, 12, 22, 11, 90]
+    sorter = BubbleSorter(nums)
+    
+    print("Before sorting")
+    sorter.display()
+    
+    sorter.sort()
+
+    print("After sorting")
+    sorter.display()
